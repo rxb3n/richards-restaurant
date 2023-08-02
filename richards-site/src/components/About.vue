8109000e2580f680563cfebcc95c36adbcff6ea4
rxb3n <61215571+rxb3n@users.noreply.github.com>
2023-08-02 11:34:50 +0200

    first commit

---
+++ richards-site/src/components/About.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- about section -->
+	<section class="action-container" id="about-section">
+		<div class="action-text">
+			<h2 class="section-title">RICHARD'S</h2>
+			<h3 class="section-sub">CUISINE FRANCAISE</h3>
+			<p class="section-text">
+				Chez Richard’s, vous pourrez déguster une cuisine traditionnelle et savoureuse, préparée avec des produits frais et de qualité. <br>
+				Que vous soyez à la recherche d’un déjeuner d’affaires, d’un dîner romantique ou d’une soirée entre amis, Richard’s vous accueille dans un cadre élégant et chaleureux.
+			</p>
+		</div>
+	</section>
+    <!-- about section -->
+</template>
+
+<script>
+
+export default{
+    name: 'About',
+    
+}
+</script>
+<style scoped>
+@import '../style.css';
+</style>
---
+++ richards-site/src/components/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+	<section id="hero-section">
+		<div class="hero-container">
+			<div class="hero-text">
+				<h2 class="hero-text-header">Bienvenue chez</h2>
+				<h1 class="hero-text-title">RICHARD'S</h1>
+				<p class="hero-description">
+					Vous cherchez un endroit où savourer la cuisine française dans une ambiance conviviale et raffinée ? <br>
+					Ne cherchez plus, vous avez trouvé : Richard’s, le restaurant français qui vous fait voyager au pays du goût. <br>
+					Chez Richard’s, vous découvrirez des plats traditionnels et délicieux, élaborés avec des produits frais et de qualité.
+				</p>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "Hero",
+};
+</script>
+
+<style scoped>
+@import "../style.css";
+</style>
